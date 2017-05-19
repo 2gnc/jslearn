b7fc41435404d70829890f7443005a84a77ede58
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2017-05-19 17:45:32 +0300

    настроен импорт фрагментов в scss файл, настроен watch для изменений в папке assets,начала верстку странички для проверятора

--- lesson1/lucky-ticket/gulpfile.js
+++ lesson1/lucky-ticket/gulpfile.js
@@ -1,17 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	pug = require('gulp-pug')
+	watch = require('gulp-watch');
 
-gulp.task('sassToCss', function(){
-	gulp.src('assets/*.scss')
+gulp.task('sassToCss', function() {
+	function run() {
+		return gulp.src('assets/*.scss', 'assets/!_*.scss') // return заставляет команды запускаться параллельно?
 		.pipe(sass())
 		.pipe(gulp.dest('lucky-ticket/style'))
+	}
+	watch('assets/*.scss', run);
+	return run();
 });
 
-gulp.task('pugToHTML', function(){
-	gulp.src('assets/*.pug')
+gulp.task('pugToHTML', function() {
+	function run() {
+		return gulp.src('assets/*.pug')
 		.pipe(pug())
 		.pipe(gulp.dest('lucky-ticket'))
+	}
+	watch('assets/*.pug', run);
+	return run();
 });
 
 
