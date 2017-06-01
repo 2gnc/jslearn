a77bfa97a5ca8d94e1538ee2bf00f1b261fc7cc3
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2017-06-01 21:54:33 +0300

    отдельные таски для pug + наблюдатель

--- homework/gulpfile.js
+++ homework/gulpfile.js
@@ -1,1 +1,12 @@
-var gulp = require('gulp');
+var gulp = require( 'gulp' ),
+	pug = require( 'gulp-pug' );
+
+gulp.task('pug', function() {
+	return gulp.src('src/**.pug')
+	.pipe(pug())
+	.pipe(gulp.dest('app/'));
+});
+
+gulp.task( 'watch', function() {
+	gulp.watch( 'src/**.pug', ['pug'] );
+});
