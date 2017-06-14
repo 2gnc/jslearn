2a1658b0c538b346fb9868ab884d8c3b4e452bdd
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-06-14 12:01:31 +0300

    добавила счетчик отказов

--- homework/app/js/scripts.js
+++ homework/app/js/scripts.js
@@ -254,12 +254,29 @@ const 	buttonAdd = document.getElementById( 'view-duration-add' ),
 // ячейка "Количество просмотров"
 		numberOfViews = document.getElementById( 'views-num' ),
 //ячейка "Процент просмотров"
-		percentOfViews = document.getElementById( 'views-percent' );
-
+		percentOfViews = document.getElementById( 'views-percent' ),
+//ячейка "Количество отказов"
+		numberOfRejects = document.getElementById( 'rejects-num' )
+;
+//Функция - обертка
 function viewsTotalCounter() {
-	var viewsCount = 0;
+//счетчик просмотров
+	var viewsCount = 0,
+//счетчик отказов
+		rejects = 0;
+//Функция - счетчик (замыкание)
 	var countTotal = function() {
-		var total = ++viewsCount;
+//Количество секунд из input
+		const	viewDuration = +document.getElementById( 'view-duration' ).value;
+//Счетчик количества просмотров (равен количеству кликов на кнопку 'Добавить')
+		var total = ++viewsCount,
+			totalRejects;
+//если это отказ, записываем отказ
+		if ( viewDuration < 10 ) {
+			totalRejects = ++rejects;
+			numberOfRejects.innerHTML = totalRejects ;
+		}
+
 		numberOfViews.innerHTML = total ;
 		percentOfViews.innerHTML = '100';
 	}
