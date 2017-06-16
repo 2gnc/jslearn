f576279f22fe029c3bb29155cd02d6d78027db9b
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-06-16 12:21:11 +0300

    готова верстка для викторины, готова база вопросов

--- homework/app/js/scripts.js
+++ homework/app/js/scripts.js
@@ -426,6 +426,12 @@ showDescription(numberToAnalize);
 'use strict';
 
 
+//Функция выдает случайное число от 1 до 50 включительно
+function getRandomArbitrary(min=1, max=51) {
+	return parseInt(Math.random() * (max - min) + min);
+}; 
+
+console.log( getRandomArbitrary() );
 'use strict';
 
 var textRedactor = function() {
--- homework/src/quiz/quiz.js
+++ homework/src/quiz/quiz.js
@@ -1,2 +1,9 @@
 'use strict';
 
+
+//Функция выдает случайное число от 1 до 50 включительно
+function getRandomArbitrary(min=1, max=51) {
+	return parseInt(Math.random() * (max - min) + min);
+}; 
+
+console.log( getRandomArbitrary() );
