cf7c85055312306a18dd2e579d69f02a6fcacf7c
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-10-10 16:30:37 +0300

    @constructor BurgerParameter added

--- homework/src/hamburger/hamburger.js
+++ homework/src/hamburger/hamburger.js
@@ -13,6 +13,9 @@
 		this.size = size;
 		this.topping = topping;
 	};
+
+
+
 	Hamburger.prototype.setSize = function() {};
 	Hamburger.prototype.addTopping = function() {};
 	Hamburger.prototype.removeTopping = function() {};
@@ -21,19 +24,27 @@
 /**
 * @constructor BurgerParameter
 * @constructs BurgerParameter
+* @param {string} type 
 * @param {string} name 
 * @param {number} price
 * @param {number} calories
 */
-	function BurgerParameter( name, price, calories ){
+	function BurgerParameter( type, name, price, calories ){
+		this.type = type;
 		this.name = name;
 		this.price = price;
 		this.calories = calories;
 	};
 
+	var s = new BurgerParameter( 'size', 'small', 50, 20 ),
+		m = new BurgerParameter( 'size', 'medium', 75, 30 ),
+		m = new BurgerParameter( 'size', 'large', 100, 40 ),
+		cheeze = new BurgerParameter( 'topping', 'cheeze', 10, 20 ),
+		salad = new BurgerParameter( 'topping', 'salad', 20, 5 ),
+		potato = new BurgerParameter( 'topping', 'potato', 10, 20 ),
+		spice = new BurgerParameter( 'topping', 'spice', 10, 20 ),
+		mayo = new BurgerParameter( 'topping', 'mayo', 10, 20 ),
+		hamb = new Hamburger( s , [] );
 
-	var hamb = new Hamburger({}, []);
 	console.log( hamb );
-
-
 })();
