173551de4082e58fe56b81bd6841c4bb6f45b686
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-10-17 17:04:53 +0300

    XMLHttpRequest added

--- homework/src/gallery/gallery.js
+++ homework/src/gallery/gallery.js
@@ -1,18 +1,28 @@
 (function(){
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'gallery.json', true);
-	xhr.send();
-	xhr.onreadystatechange = function() { 
-		if (xhr.status != 200) {
-			console.log( 'ошибка' );
-		}
-		if (xhr.readyState != 4) { 
-			return 
-		}
-	};
-	console.log( xhr );
+window.addEventListener( 'load', getGalleryLinks );
 
+	function getGalleryLinks() {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(e) {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					galleryBuild();
+			} else {
+			return
+			};
+		};
+			xhr.ontimeout = function () {
+			console.log( 'Ожидание ответа заняло слишком много времени' );
+			};
+		};
+		xhr.open('get', 'gallery.json', true)
+		xhr.send();
+		function galleryBuild() {
+			var links = JSON.parse( xhr.responseText );
+			console.log( links.links );
+		};
+	};
 
 
 })();
