1f4dc159e61970909918db1cee6cd7b384fa0c55
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2017-11-07 15:33:05 +0300

    task2 added

--- reacthomework/lesson1/script.js
+++ reacthomework/lesson1/script.js
@@ -33,21 +33,31 @@
 // 	 по которым было произведено вычисление.
 
 (function() {
-	const calculateArea = function( a, b, c ) {
+	const calculateArea = function( a, b, c, d ) {
 		let figure = '',
 				area = 0,
 				input = [],
-				args = arguments.length;
+				p = 0,
+				p1 = 0,
+				args = arguments.length,
+				obj = {};
 		// проверить, числа ли во входе. Если нет - перобразовать, если не получилось - выйти с ошибкой. 
 		for ( var i = 0; i < args; i++ ) {
 			if( Number(arguments[i]) ) {
 				input.push( Number(arguments[i]) )
 			}
 		};
-		if ( input.length !== args ) {
-			console.log( 'err' );
+		if ( input.length !== args || input.length > 4 ) {
+			console.log( 'слишком много сторон или некорректный формат' );
 			input = [];
-			return
+			return obj = {
+				name: 'неизвестно',
+				area: 'неизвестно',
+				input: 'некорректный ввод',
+				print: function() {
+				console.log( 'Фигура: ' + this.name + ', Площадь: ' + this.area + ', Стороны:' + this.input )
+				}
+			}
 		}
 		switch( args ) {
 			case 0: 
@@ -63,17 +73,39 @@
 				figure = 'прямоугольник';
 				break;
 			case 3:
-				let p = ( a + b + c )*0.5;
+				p = a/2 + b/2 + c/2;
 				area = Math.sqrt( p * ( p - a )*( p - b ) * ( p - c ) ).toFixed(2);
 				figure = 'треугольник';
 				break;
+			default :
+				p1 = a/2 + b/2 + c/2 + d/2;
+				area = Math.sqrt( ( p - a )*( p - b ) * ( p - c ) *  ( p - d ) ).toFixed(2);
+				figure = 'четырехугольник';;
+				break;
 		}
-		console.log( figure );
-		console.log( input );
-		console.log( area );
-
-	}
-
-	calculateArea('4', '5');
-
+		return obj = {
+			name: figure,
+			area: area,
+			input: input.join(', '),
+			print: function() {
+				console.log( 'Фигура: ' + this.name + ', Площадь: ' + this.area + ', Стороны: ' + this.input )
+			}
+		};
+	} 
+	calculateArea(2, 3).print();
 })();
+
+// 3. Необходимо написать иерархию классов вида:
+// Human -> Employee -> Developer
+// Human -> Employee -> Manager
+// Каждый Менеджер (Manager) должен иметь внутренний массив своих сотрудников (разработчиков), 
+// а также методы по удалению/добавлению разработчиков.
+// Каждый Разработчик (Developer) должны иметь ссылку на Менеджера и методы для изменения менеджера 
+// (имеется ввиду возможность назначить другого менеджера).
+// У класса Human должны быть следующие параметры: name (строка), age (число), dateOfBirth (строка или дата)
+// У класса Employee должны присутствовать параметры: salary (число), department (строка)
+// В классе Human должен присутствовать метод displayInfo, который возвращает строку со всеми параметрами экземпляра Human.
+// В классе Employee должен быть реализовать такой же метод (displayInfo), который вызывает базовый метод и дополняет 
+// его параметрами из экземпляра Employee
+// Чтобы вызвать метод базового класса, необходимо внутри вызова метода displayInfo класса Employee 
+// написать: super.displayInfo(), это вызовет метод disaplyInfo класс Human и вернет строку с параметрами Human'a.
