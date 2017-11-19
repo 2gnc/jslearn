12ffca9e2e11e9454bcfafecd0f21d5ca3f5e206
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2017-11-19 21:56:56 +0300

    третий урок (продолжить с 0:46)

---
+++ reacthomework/lesson3/inLesson/app/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOOM from 'react-dom';
+import Menu from './components/menu/Menu.js'
+
+ReactDOOM.render( <Menu/>, document.getElementById( 'app' ) );
---
+++ reacthomework/lesson3/inLesson/app/components/Menu/MenuItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MenuItem extends React.Component {
+	render() {
+		return <li>Главная</li>;
+	}
+};
